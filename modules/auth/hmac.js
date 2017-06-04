4c7050c4ea8984024530c89fc7dcfd13231ab8c6
ep-friedel <friedel@epages.com>
2017-06-04 14:18:30 +0000

    fix hmac - crypto.hmac is singleuse

--- modules/auth/hmac.js
+++ modules/auth/hmac.js
@@ -1,11 +1,10 @@
 const crypto = require('crypto');
 
 module.exports = (secret, hashfield) => {
-    let hmac = crypto.createHmac('SHA1', secret);
 
     return (req, res, next) => {
         if (req.body) {
-            let hash = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+            let hash = 'sha1=' + crypto.createHmac('SHA1', secret).update(JSON.stringify(req.body)).digest('hex');
 
             if (req.get(hashfield) === hash) {
                 next();
