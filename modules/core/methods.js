c05ac2b51e6a5ce2be009ef3f25712debdc53270
ep-friedel <friedel@epages.com>
2017-09-10 13:20:01 +0000

    fix copyright obfuscation translation nations

--- modules/core/methods.js
+++ modules/core/methods.js
@@ -527,7 +527,7 @@ function methods (back) {
         } else if (html.indexOf('<article') !== -1) {
             retvar = html.split(/<article.*?>/)[1]
                         .split('</article>')[0]
-                        .replace(/<span class="copyright-obfuscation">[^<]*<\/span>/, '')
+                        .replace(/<span class="copyright-obfuscation">[^<]*<\/span>/g, '')
                         .replace(/<p.*?<a.*?ext Chapter.*?\/p>/, '')
                         .split(/<p[^<]*?<a.*?ext Chapter.*?\/p>/g)[0]
                         .replace(/<script.*?\/script>/, '')
